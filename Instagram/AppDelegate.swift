a35410865f2629452410221b24859da8440e2163
Omar Valenzuela <ovalenzu@uci.edu>
2018-08-21 18:17:45 -0700

    logout successfully makes PFUser.current() == nil

--- Instagram/AppDelegate.swift
+++ Instagram/AppDelegate.swift
@@ -28,7 +28,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         if PFUser.current() != nil {
             let storyboard = UIStoryboard(name: "Main", bundle: nil)
             // view controller currently being set in Storyboard as default will be overridden
-            window?.rootViewController = storyboard.instantiateViewController(withIdentifier: "HomeViewController")
+            window?.rootViewController = storyboard.instantiateViewController(withIdentifier: "InstagramTabViewController")
         }
 
         
